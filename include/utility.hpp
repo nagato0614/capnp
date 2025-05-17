3f2d1c1fef36131f7c11d6f6c113366c890710d5
Toru Nagai <nagato0614@gmail.com>
2025-05-17 10:58:47 +0900

    ♻️ リファクタリング: プロジェクト名をRepeatingTimerExampleからcapnproto_examplesに変更 ✨ 新機能: timer_exampleの実装を追加 🗑️ 不要なファイル(client.cpp, onchange_impl.hpp, server.cpp)を削除 🔧 設定: .gitignoreの重複エントリを削除 ✨ 新機能: utility.hppを追加してログ機能を提供

---
+++ include/utility.hpp
@@ -0,0 +1,38 @@
+//
+// Created by toru on 2025/05/17.
+//
+#define KJ_LOG_LEVEL KJ_LOG_SEVERITY_INFO
+#include <kj/async-io.h>
+#include <kj/debug.h>
+#include <kj/std/iostream.h>
+
+#include <chrono>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#ifndef UTILITY_HPP
+#define UTILITY_HPP
+
+std::string get_current_time_string() {
+  using namespace std::chrono;
+
+  auto now = system_clock::now();
+  auto in_time_t = system_clock::to_time_t(now);
+  auto fractional = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+  std::tm local_time = *std::localtime(&in_time_t);
+
+  std::ostringstream oss;
+  oss << "[" << std::put_time(&local_time, "%H:%M:%S") << '.' << std::setw(3)
+      << std::setfill('0') << fractional.count() << "]";
+
+  return oss.str();
+}
+
+#define LOG_COUT                                                               \
+std::cout << get_current_time_string() << "[" << __FILE__ << ":" << __LINE__ \
+<< "]"
+
+#endif //UTILITY_HPP
